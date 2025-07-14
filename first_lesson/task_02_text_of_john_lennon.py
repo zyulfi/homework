35e87c449ad54fda8a45ca58e3cf5dca0344121f
zyulfi <zyulfigugov@gmail.com>
2025-07-14 21:38:24 +0300

    text of John Lennon

---
+++ first_lesson/task_02_text_of_john_lennon.py
@@ -0,0 +1,12 @@
+#Изведете на екрана "Life is what happens when you're busy making other plans"
+#John Lennon на различни редове.
+#Примерен изход:
+#"Life is what happens
+#    when
+        #you're busy making other plans"
+            #John Lennon
+
+print("Life is what happens")
+print("    " + "when")
+print("    " + "    " + "you're busy making other plans")
+print("    " + "    " + "    " + "John Lennon")
