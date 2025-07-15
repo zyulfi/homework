cdb3f02fe44b3268c9c5b79b6ac7e41e90c2520f
zyulfi <zyulfigugov@gmail.com>
2025-07-15 11:15:38 +0300

    Calculates the sum and multiplication of three numbers

--- second_lesson/task01_sum_multipl_num.py
+++ second_lesson/task01_sum_multipl_num.py
@@ -3,9 +3,9 @@
 # на числата.
 # Резултатът от изчисленията трябва да се изведе на екрана.
 
-num_one = float(input("Please, enter first number: "))
-num_second = float(input("Please, enter second number: "))
-num_third = float(input("Please, enter third number: "))
+num_one = float(input("Please enter first number: "))
+num_second = float(input("Please enter second number: "))
+num_third = float(input("Please enter third number: "))
 
 num_sum = num_one + num_second + num_third
 num_multiplication = num_one * num_second * num_third
