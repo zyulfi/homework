3b78f1a24c96526a98a645ef65fa8ae8cc13e868
zyulfi <zyulfigugov@gmail.com>
2025-07-18 06:46:46 +0300

    Converts meters to centimeters decimeters milimeters and miles

---
+++ second_lesson/task06_units_of_meansurement.py
@@ -0,0 +1,16 @@
+#Задача 3
+#Потребител въвежда от клавиатурата броя на метрите.
+#Изходът трябва да е резултат от преобразуването на метрите в сантиметри, дециметри, милиметри и мили.
+
+meters = float(input("Моля, въведете броя на метрите: "))
+
+centimeters = meters * 100
+decimeters = meters * 10
+millimetres = meters * 1000
+miles = meters * 1610
+
+print("Тhe entered " + str(meters) + " meters are:" + "\n"
+      + str(centimeters) + " centimeters" "\n"
+      + str(decimeters) + " decimeters" "\n"
+      + str(millimetres) + " millimeters" "\n"
+      + str(miles) + " miles")
